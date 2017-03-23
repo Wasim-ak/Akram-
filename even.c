73346d5b27c8bdabcaa48fc53c1d1dbabb58da5f
Wasim-ak <wasiakram1996@gmail.com>
2017-03-24 01:03:48 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("Enter a starting number:");
+scanf("%d",&a);
+printf("Enter the ending number:");
+scanf("%d",&b);
+for(a;a<b;a++)
+{
+if(a%2==0)
+printf("%d\t",a);
+}
+return 0;
+}
