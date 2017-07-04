1af64e366ab597be4e143cb2629ca28bee09c7c8
Wasim-ak <wasiakram1996@gmail.com>
2017-07-05 00:16:53 +0530

    Add files via upload

---
+++ pro_51.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("\nEnter the number of piece in chocolate bars:");
+scanf("%d%d",&a,&b);
+printf("No of Times chocolate bars to be spit is:%d",(a*b)-1);
+}
